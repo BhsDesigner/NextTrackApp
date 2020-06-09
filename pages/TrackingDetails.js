bb5ac8ceebcaf7e58070873b2477f5668cf89275
BhsDesigner <manoj@homescapesonline.com>
2020-06-09 13:19:24 +0530

    your order delivered hidden

--- pages/TrackingDetails.js
+++ pages/TrackingDetails.js
@@ -12,11 +12,11 @@ const TrackingDetails = ({tracking}) => {
      const classes = useStyles();
      return(
              <div>
-             <Container component="main" maxWidth="md" className={classes.DetailsContainer}>
+                 {/*<Container component="main" maxWidth="md" className={classes.DetailsContainer}>
                  <Grid container className={classes.OrderStatusBox}>
                      <Typography variant="h3" className={classes.h3}>Your order is delivered</Typography>
                  </Grid>
-             </Container>
+             </Container> */}
 
              
              <CourierDetails tracking={tracking}/>
